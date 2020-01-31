7d5f03ec26e09f6457aae770f904069423183149
vanderlindeni0104 <vanderlindeni0104@CA-SU-F106-19.sduhsd.lan>
2020-01-31 10:47:52 -0800

    1/31 end commit

--- APCSA-2020/src/unit0/Variables.java
+++ APCSA-2020/src/unit0/Variables.java
@@ -11,32 +11,39 @@ public class Variables
 {
 	public static void main ( String[] args )
 	{
-		//define 1 variable of each of the
-		//following data types
-		//byte		short		int 		long
-		//float		double
-		//char      boolean		String
-
-		//integer variables
+		int Two = 4;
+		short Three = 20;
+		float Four = 0.001f;
+		double Cat = 4.4;
+		char Dog = 'a';
+		long Hat = 44444444;
+		boolean tf = true;
+		String Worm = "Hello world";
 		byte byteOne = 127;
 
-
-		//decimal variables
-
-
-		//other integer types
-
-
-		//other types
-
-
-		//output your information here
-	
-
-
-
-
-
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Ilona            1/31/20                            *");
+		System.out.println("*                                                             *");
+		System.out.println("*        integer types                              *");
+		System.out.println("*                                                             *");
+		System.out.println("*8 bit - Byte = "+byteOne+"\t\t         *");
+		System.out.println("*16 bit - Short = "+Three+"\t\t         *");
+		System.out.println("*32 bit - Int = "+Two+"\t\t                 *");
+		System.out.println("*64 bit - Long = "+Hat+      "\t\t        *");
+		System.out.println("*                                                             *");
+		System.out.println("*        real types                                   *");
+		System.out.println("*                                                             *");
+		System.out.println("*32 bit - Float  = "+Four+"\t\t           *");
+		System.out.println("*64 bit - Double = "+Cat+"\t\t           *");
+		System.out.println("*                                                             *");
+		System.out.println("*        other integer types                    *");
+		System.out.println("*                                                             *");
+		System.out.println("*16 bit - Char = "+Dog+"\t\t               *");
+		System.out.println("*                                                             *");
+		System.out.println("*        other types                                 *");
+		System.out.println("*                                                             *");
+		System.out.println("Boolean =  "+tf+"\t\t                           *");
+		System.out.println("String =  "+Worm+"\t\t                        *");
 
 
 
