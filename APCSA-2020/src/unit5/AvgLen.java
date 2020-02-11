f768867abaafdc775aee4d04f9116c9e376f8c25
vanderlindeni0104 <vanderlindeni0104@CA-SU-F106-19.sduhsd.lan>
2020-02-11 11:07:02 -0800

    committed

---
+++ APCSA-2020/src/unit5/AvgLen.java
@@ -0,0 +1,38 @@
+package unit5;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+	   char start = 1;
+	   char lasta = a.charAt(a.length() - 1);
+	   char lastb = b.charAt(b.length() - 1);
+	   int yesa = 0;
+	   int yesb = 0;
+	   
+	  switch (lasta) {
+	  case 'a':
+	  case 'e':
+	  case 'i':
+	  case 'o':
+	  case 'u':
+	  case 'A':
+	  case 'E':
+	  case 'I':
+	  case 'O':
+	  case 'U':
+	   
+	  }
+		   
+	   
+	}
+}
