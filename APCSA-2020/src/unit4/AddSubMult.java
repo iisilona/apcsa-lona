f2a28d5f47b770c54bbfc7682d7361c3f236de66
vanderlindeni0104 <vanderlindeni0104@CA-SU-F106-19.sduhsd.lan>
2020-02-11 10:02:51 -0800

    h e w w o

---
+++ APCSA-2020/src/unit4/AddSubMult.java
@@ -0,0 +1,25 @@
+package unit4;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*; 
+import java.util.Scanner;
+
+public class AddSubMult
+{
+	public static double check( double a, double b )
+	{
+		if (a > b) {
+			return (a - b);
+		}
+		if(b > a) {
+			return (b - a);
+		}
+		if(b == a) {
+			return (b * a);
+		}
+		return 0;
+	}
+}
