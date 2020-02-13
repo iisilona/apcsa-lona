b13fb051075173646f7a72330821f993fef8bb9f
vanderlindeni0104 <vanderlindeni0104@CA-SU-F106-19.sduhsd.lan>
2020-02-13 10:20:45 -0800

    here

--- APCSA-2020/src/unit5/StringEquality.java
+++ APCSA-2020/src/unit5/StringEquality.java
@@ -6,30 +6,46 @@
 //Class -
 //Lab  -
 
-import static java.lang.System.*;
+package unit5;
+//(c) A+ Computer Science
+//www.apluscompsci.com
 
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
 public class StringEquality
 {
 	private String wordOne, wordTwo;
 
-	public StringEquality()
-	{
-	}
-
 	public StringEquality(String one, String two)
 	{
-	}
+		setWords(one,two);
+		checkEquality();
+     }
 
 	public void setWords(String one, String two)
 	{
+		wordOne =  one;
+		wordTwo = two;
 	}
 
 	public boolean checkEquality( )
 	{
-		return false;
+			if(wordOne.equals(wordTwo)) {
+				return true;
+			}
+			else return false;
 	}
 
-	public String toString()
+	public String toStringtrue()
+	{
+		return wordOne + " has the same letters as " + wordTwo + "\n";
+	}
+	
+	public String toStringfalse()
 	{
 		return wordOne + " does not have the same letters as " + wordTwo + "\n";
 	}
--- APCSA-2020/src/unit5/StringEqualityRunner.java
+++ APCSA-2020/src/unit5/StringEqualityRunner.java
@@ -1,10 +1,11 @@
+package unit5;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
 //Name -
 //Date -
 //Class -
-//Lab  - 
+//Lab  -
 
 import static java.lang.System.*;
 
@@ -12,7 +13,25 @@ public class StringEqualityRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases
-		
+	 String one = "Cat";
+     String two = "Dog";
+     String three = "Bird";
+     String four = "Bird";
+     
+		StringEquality test = new StringEquality(one, two);
+		{
+		if(test.checkEquality()) {
+			 System.out.println(test.toStringtrue());
+		 }
+		 else System.out.println(test.toStringfalse());
+		}
+	
+	StringEquality testtwo = new StringEquality(three, four);
+	{
+	 if(test.checkEquality()) {
+		 System.out.println(testtwo.toStringtrue());
+	 }
+	 else System.out.println(testtwo.toStringfalse());
 	}
+ }
 }
