46e36e25807ba4a7eae01f15853636c02c368fcd
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-02-17 09:30:31 -0800

    from home

--- APCSA-2020/src/unit5/WordsCompare.java
+++ APCSA-2020/src/unit5/WordsCompare.java
@@ -11,39 +11,23 @@ import static java.lang.System.*;
 public class WordsCompare
 {
 	private String wordOne, wordTwo;
-   private int compare;
-
-	public WordsCompare()
-	{
+	private int compare;
+	
+	public WordsCompare(String one, String twp) {
+		setWords(one, two);
 	}
-
-	public WordsCompare(String one, String two)
-	{
-		for (int length = one.length()) {
-			
-		}
+	
+	public void setWords(one, two) {
+		wordOne = one;
+		wordTwo = two;
 	}
-
-	public void setWords(String one, String two)
-	{
-     wordOne = one;
-     wordTwo = two;
+	
+	public void compare() {
+	      wordOne.compareTo(wordTwo);
 	}
-
-	public void compare()
-	{
-		compare = wordOne.compareTo(wordTwo);
-	}
-
-	public String toString()
-	{
-		if(compare<0)
-		{
-			return wordOne + " should be placed before " + wordTwo + "\n";
-		}
-		else
-		{
-			return wordOne + " should be placed after " + wordTwo + "\n";
-		}
-	}
-}
+	
+	public String toString() {
+		if (compare < 0)
+			return a + " should be placed before " + b + "\n";
+		 else
+			return a + " should be placed after " + b + "\n";
