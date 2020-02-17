814ff976efa86441b9ba690212849cee82194698
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-02-17 09:44:08 -0800

    finished, just fix the runner

--- APCSA-2020/src/unit6/CountPairs.java
+++ APCSA-2020/src/unit6/CountPairs.java
@@ -10,6 +10,11 @@ public class CountPairs
 	public static int pairCounter( String str )
 	{ 
 		int count = 0;
+		for (int index = 1; index <= str.length() - 1; index++) {
+			if (str.charAt(index) == str.charAt(index - 1))
+				count++;
+		}
 		return count;
+			
 	}
 }
