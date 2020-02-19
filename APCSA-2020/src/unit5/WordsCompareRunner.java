6e9fe16f2fda43d6a1c05db8a218566be2f865a7
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-02-18 18:14:35 -0800

    Update WordsCompareRunner.java

--- APCSA-2020/src/unit5/WordsCompareRunner.java
+++ APCSA-2020/src/unit5/WordsCompareRunner.java
@@ -13,5 +13,22 @@ public class WordsCompareRunner
    public static void main( String args[] )
    {
 		wordsCompare s = new wordsCompare();
+	        s.setWords("abe", "ape");
+		System.out.println(s.toString());
+	        s.setWords("giraffe", "gorilla");
+		System.out.println(s.toString());
+	        s.setWords("one", "two");
+		System.out.println(s.toString());
+	        s.setWords("fun", "funny");
+		System.out.println(s.toString());
+	        s.setWords("123", "19");
+		System.out.println(s.toString());
+	        s.setWords("193", "1910");
+		System.out.println(s.toString());
+	        s.setWords("goofy", "godfather");
+		System.out.println(s.toString());
+	        s.setWords("funnel", "fun");
+		System.out.println(s.toString());
+	        
 	}
 }
