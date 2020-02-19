76eaa0ad301cc2232e32cedb1f0385ed66afb8bd
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-02-18 21:44:32 -0800

    finished

--- APCSA-2020/src/unit6/PerfectRunner.java
+++ APCSA-2020/src/unit6/PerfectRunner.java
@@ -9,7 +9,26 @@ public class PerfectRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases
-																
+		Perfect perf = new Perfect();
+		perf.setNumber(496);
+		System.out.println(perf.toString());
+		perf.setNumber(45);
+		System.out.println(perf.toString());
+		perf.setNumber(6);
+		System.out.println(perf.toString());
+		perf.setNumber(14);
+		System.out.println(perf.toString());
+		perf.setNumber(8128);
+		System.out.println(perf.toString());
+		perf.setNumber(1245);
+		System.out.println(perf.toString());
+		perf.setNumber(33);
+		System.out.println(perf.toString());
+		perf.setNumber(28);
+		System.out.println(perf.toString());
+		perf.setNumber(27);
+		System.out.println(perf.toString());
+		perf.setNumber(33550336);
+		System.out.println(perf.toString());
 	}
 }
