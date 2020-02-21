a017e31cce292a81913e31cd58d4deb542a8e2bb
vanderlindeni0104 <vanderlindeni0104@CA-SU-F106-19.sduhsd.lan>
2020-02-21 10:26:50 -0800

    unit 8

---
+++ APCSA-2020/src/unit8/RaySumLast.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class RaySumLast
+{
+	public static int go(int[] ray)
+	{
+		return 0;
+	}
+}
---
+++ APCSA-2020/src/unit8/SumLastRunner.java
@@ -0,0 +1,11 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class SumLastRunner
+{
+	public static void main( String args[] )
+	{											
+	}
+}
