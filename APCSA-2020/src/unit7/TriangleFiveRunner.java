30b942238917700e539ec9e4947a11fa83f11f26
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-02-22 21:38:44 -0800

    done, make sure test cases are accurate

--- APCSA-2020/src/unit7/TriangleFiveRunner.java
+++ APCSA-2020/src/unit7/TriangleFiveRunner.java
@@ -6,9 +6,30 @@ package unit7;
 
 import static java.lang.System.*;
 
+
 public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
+	   TriangleFive tri = new TriangleFive();
+	   tri.setAmount(4);
+	   tri.setLetter('C');
+	   System.out.println(tri);
+	   
+	   tri.setAmount(5);
+	   tri.setLetter('A');
+	   System.out.println(tri);
+	   
+	   tri.setAmount(7);
+	   tri.setLetter('B');
+	   System.out.println(tri);
+	   
+	   tri.setAmount(6);
+	   tri.setLetter('X');
+	   System.out.println(tri);
+	   
+	   tri.setAmount(8);
+	   tri.setLetter('Z');
+	   System.out.println(tri);
 	}
 }
