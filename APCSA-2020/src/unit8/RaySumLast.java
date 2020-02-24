770318900d2e5a89e08f100c679e346dc1efddd2
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-02-23 20:00:31 -0800

    done

--- APCSA-2020/src/unit8/RaySumLast.java
+++ APCSA-2020/src/unit8/RaySumLast.java
@@ -1,12 +1,17 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-//Date -
-
 public class RaySumLast
 {
 	public static int go(int[] ray)
 	{
-		return 0;
+		int temp = ray[ray.length-1];
+		int sum = 0;
+		for (int i = 0; i <= ray.length-1; i++) {
+			if (ray[i] > temp) {
+				sum += ray[i];
+			}
+		}
+		if (sum == 0) {
+			return -1;
+		}
+		return sum;
 	}
 }
