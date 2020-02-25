7cbb675ae399ccd7238ab8dfc42ae6eb410f6dfe
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-02-24 20:54:25 -0800

    done

--- APCSA-2020/src/unit8/RayDown.java
+++ APCSA-2020/src/unit8/RayDown.java
@@ -5,10 +5,14 @@
 
 public class RayDown
 {
-	//go() will return true if all numbers in numArray
-	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
 	{
+		int val;
+		for (int i =1; i <= numArray.length -1; i++) {
+			val = numArray[i-1];
+			if (val <= numArray[i]) {
+				return false;
+			}
+		}
 		return true;
 	}	
-}
