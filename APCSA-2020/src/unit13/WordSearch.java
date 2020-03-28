2c97fe8a4371c68902e134762bcb7d686ae1394b
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-03-27 21:41:27 -0700

    ye

---
+++ APCSA-2020/src/unit13/WordSearch.java
@@ -0,0 +1,66 @@
+package Unit13;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.util.*;
+import java.io.*; 
+
+public class WordSearch
+{
+    private String[][] m;
+
+    public WordSearch( int size, String str )
+    {
+    }
+
+    public boolean isFound( String word )
+    {
+    	return false;
+    }
+
+	public boolean checkRight(String w, int r, int c)
+   {
+		return false;
+	}
+
+	public boolean checkLeft(String w, int r, int c)
+	{
+		return false;
+	}
+
+	public boolean checkUp(String w, int r, int c)
+	{
+		return false;
+	}
+
+	public boolean checkDown(String w, int r, int c)
+   {
+	   return false;
+	}
+
+	public boolean checkDiagUpRight(String w, int r, int c)
+	{
+		return false;
+	}
+
+	public boolean checkDiagUpLeft(String w, int r, int c)
+	{
+		return false;
+	}
+
+	public boolean checkDiagDownLeft(String w, int r, int c)
+   {
+		return false;
+	}
+
+	public boolean checkDiagDownRight(String w, int r, int c)
+	{
+		return false;
+	}
+
+    public String toString()
+    {
+ 		return "";
+    }
+}
