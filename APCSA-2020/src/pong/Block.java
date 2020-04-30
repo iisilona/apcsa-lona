6569dfd82a223b5b8c9624d995d537cc98b2d25c
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-04-30 13:54:03 -0700

    Create Block.java

---
+++ APCSA-2020/src/pong/Block.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Block implements Locatable
+{
+	private int xPos;
+	private int yPos;
+	private int width;
+	private int height;
+
+	private Color color;
+
+	public Block()
+	{
+
+
+	}
+
+	//add other Block constructors - x , y , width, height, color
+	
+	
+	
