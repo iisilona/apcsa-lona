d2d7b86f05154bfd3dba9cc9ccf6105feca21e0f
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-04-30 13:52:41 -0700

    Create Paddle.java

---
+++ APCSA-2020/src/pong/Paddle.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Paddle extends Block
+{
+   //instance variables
+   private int speed;
+
+   public Paddle()
+   {
+		super(10,10);
+      speed =5;
+   }
+
+
+   //add the other Paddle constructors
+
+
+
+
+
+
+
+
+
+
+   public void moveUpAndDraw(Graphics window)
+   {
+
+
+   }
+
+   public void moveDownAndDraw(Graphics window)
+   {
+
+
+   }
+
+   //add get methods
+   
+   
+   //add a toString() method
+}
