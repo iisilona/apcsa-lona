d3546616ab66c721c8f9ab1c90346c30f91bfb27
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-04-30 14:05:27 -0700

    Create BlinkyBall.java

---
+++ APCSA-2020/src/pong/BlinkyBall.java
@@ -0,0 +1,54 @@
+class BlinkyBall extends Ball
+{
+
+   //constructors
+   public BlinkyBall()
+   {
+		super();
+   }
+
+   public BlinkyBall(int x, int y)
+   {
+
+
+
+   }
+
+   public BlinkyBall(int x, int y, int wid, int ht)
+   {
+
+
+
+   }
+
+   public BlinkyBall(int x, int y, int wid, int ht, int xSpd, int ySpd)
+   {
+
+
+
+   }
+
+   public BlinkyBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
+   {
+
+
+
+
+   }
+
+   public Color randomColor()
+   {
+   	int r = 0;		//use Math.random()
+ 		int g = 0;
+ 		int b = 0;
+ 		return new Color(r,g,b);
+   }
+
+   public void moveAndDraw(Graphics window)
+   {
+
+
+
+
+   }
+}
