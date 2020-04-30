8bc5ab7eb99d66f20f18d80dd32c4469b1533ad3
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-04-30 14:07:03 -0700

    Create Ball.java

---
+++ APCSA-2020/src/pong/Ball.java
@@ -0,0 +1,17 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Ball extends Block
+{
+	private int xSpeed;
+	private int ySpeed;
+
+	public Ball()
+	{
+		super(200,200);
+		xSpeed = 3;
+		ySpeed = 1;
+	}
+
+	//add the other Ball constructors
