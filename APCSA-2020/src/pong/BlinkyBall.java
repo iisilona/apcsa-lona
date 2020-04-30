30ea536dac8372416cc973484939e6c259827e78
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-04-30 14:18:48 -0700

    Update BlinkyBall.java

--- APCSA-2020/src/pong/BlinkyBall.java
+++ APCSA-2020/src/pong/BlinkyBall.java
@@ -1,3 +1,7 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
 class BlinkyBall extends Ball
 {
 
@@ -9,46 +13,37 @@ class BlinkyBall extends Ball
 
    public BlinkyBall(int x, int y)
    {
-
-
-
+	   super(x, y);
    }
 
    public BlinkyBall(int x, int y, int wid, int ht)
    {
-
-
-
+	   super(x, y, wid, ht);
    }
 
    public BlinkyBall(int x, int y, int wid, int ht, int xSpd, int ySpd)
    {
-
-
-
+	   super(x, y, wid, ht, xSpd, ySpd);
    }
 
    public BlinkyBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
    {
-
-
-
-
+	   super(x, y, wid, ht, col, xSpd, ySpd);
    }
 
    public Color randomColor()
    {
-   	int r = 0;		//use Math.random()
- 		int g = 0;
- 		int b = 0;
+   		int r = (int)(Math.random()*256);		//use Math.random()
+ 		int g = (int)(Math.random()*256);
+ 		int b = (int)(Math.random()*256);
  		return new Color(r,g,b);
    }
 
    public void moveAndDraw(Graphics window)
    {
-
-
-
-
+	   draw(window, Color.WHITE);
+	   setX(getX()+getXSpeed());
+	   setY(getY()+getYSpeed());
+	   draw(window, randomColor());
    }
 }
