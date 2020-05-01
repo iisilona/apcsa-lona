f17e41cb6ac45ebcdb29b1e08d1b4a7ee8d19de5
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-05-01 15:29:46 -0700

    Update SpeedUpBall.java

--- APCSA-2020/src/pong/SpeedUpBall.java
+++ APCSA-2020/src/pong/SpeedUpBall.java
@@ -2,50 +2,47 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-class SpeedUpBall extends Ball
 {
-
-   //instance variables
 
    public SpeedUpBall()
    {
-
-
+	   super();
    }
 
    public SpeedUpBall(int x, int y)
    {
-
-
+	   super(x, y);
    }
 
 
    public SpeedUpBall(int x, int y, int xSpd, int ySpd)
    {
-
-
+	   super(x, y);
+	   setSpeed(xSpd, ySpd);
    }
 
    public SpeedUpBall(int x, int y, int wid, int ht, int xSpd, int ySpd)
    {
-
-
+	   super(x, y, wid, ht, xSpd, ySpd);
    }
-
 
    public SpeedUpBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
    {
-
-
-
-
+	   super(x, y, wid, ht, col, xSpd, ySpd);
    }
 
    public void setXSpeed( int xSpd )
    {
+	   if (xSpd<0) super.setXSpeed(xSpd-1);
+	   else super.setXSpeed(xSpd+1);
+   }
 
-
-
+   public void setYSpeed( int ySpd )
+   {
+	   if (ySpd<0) super.setYSpeed(ySpd-1);
+	   else super.setYSpeed(ySpd+1);
+   }
+}
 
    }
 
