3836df2012a6fb4104d485d0ffd88d917f58f0a5
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-05-26 13:08:41 -0700

    Update MovingThing.java

--- APCSA-2020/src/starfighter/MovingThing.java
+++ APCSA-2020/src/starfighter/MovingThing.java
@@ -30,52 +30,56 @@ public abstract class MovingThing implements Moveable
 
 	public MovingThing(int x, int y, int w, int h)
 	{
-		//add code here
+		xPos = x;
+		yPos = y;
+		width = w;
+		height = h;
 	}
 
 	public void setPos( int x, int y)
 	{
-		//add code here
+		xPos = x;
+		yPos = y;
 	}
 
 	public void setX(int x)
 	{
-		//add code here
+		xPos = x;
 	}
 
 	public void setY(int y)
 	{
-		//add code here
+		yPos = y;
 	}
 
 	public int getX()
 	{
-		return 0;   //finish this method
+		return xpos;
 	}
 
 	public int getY()
 	{
-		return 0;  //finish this method
+		return ypos;
 	}
 
 	public void setWidth(int w)
 	{
-		//add code here
+		width = w;
 	}
 
 	public void setHeight(int h)
 	{
-		//add code here
+		height = h;
 	}
 
 	public int getWidth()
 	{
-		return 0;  //finish this method
+		return width;
 	}
 
 	public int getHeight()
 	{
-		return 0;  //finish this method
+		return height; 
 	}
 
 	public abstract void move(String direction);
