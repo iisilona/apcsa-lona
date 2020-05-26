eb476123bcfaa3d5477ab375dea71e09488ffc2c
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-05-26 12:26:51 -0700

    Create Ship.java

---
+++ APCSA-2020/src/starfighter/Ship.java
@@ -0,0 +1,72 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.io.File;
+import java.net.URL;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+
+public class Ship extends MovingThing
+{
+	private int speed;
+	private Image image;
+
+	public Ship()
+	{
+		this(10,10,10,10,10);
+	}
+
+	public Ship(int x, int y)
+	{
+	   //add code here
+	}
+
+	public Ship(int x, int y, int s)
+	{
+	   //add code here
+	}
+
+	public Ship(int x, int y, int w, int h, int s)
+	{
+		super(x, y, w, h);
+		speed=s;
+		try
+		{
+			URL url = getClass().getResource("/images/ship.jpg");
+			image = ImageIO.read(url);
+		}
+		catch(Exception e)
+		{
+			//feel free to do something here
+		}
+	}
+
+
+	public void setSpeed(int s)
+	{
+	   //add more code
+	}
+
+	public int getSpeed()
+	{
+	   return 0;
+	}
+
+	public void move(String direction)
+	{
+		//add code here
+	}
+
+	public void draw( Graphics window )
+	{
+   	window.drawImage(image,getX(),getY(),getWidth(),getHeight(),null);
+	}
+
+	public String toString()
+	{
+		return super.toString() + getSpeed();
+	}
+}
