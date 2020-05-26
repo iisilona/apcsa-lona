f544779debc226d44e029e1d926f46b017e69459
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-05-26 11:58:33 -0700

    Create Ammo.java

---
+++ APCSA-2020/src/starfighter/Ammo.java
@@ -0,0 +1,55 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class Ammo extends MovingThing
+{
+	private int speed;
+
+	public Ammo()
+	{
+		this(0,0,0);
+	}
+
+	public Ammo(int x, int y)
+	{
+		//add code
+	}
+
+	public Ammo(int x, int y, int s)
+	{
+		//add code
+	}
+
+	public void setSpeed(int s)
+	{
+	   //add code
+	}
+
+	public int getSpeed()
+	{
+	   return 0;
+	}
+
+	public void draw( Graphics window )
+	{
+		//add code to draw the ammo
+	}
+	
+	
+	public void move( String direction )
+	{
+		//add code to draw the ammo
+	}
+
+	public String toString()
+	{
+		return "";
+	}
+}
