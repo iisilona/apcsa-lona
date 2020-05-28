80bc431487eb38a35a3c61186aa9a9f45e6a8c12
ilona vdlin ♡ <50212229+iisilona@users.noreply.github.com>
2020-05-27 17:17:04 -0700

    Update AlienHorde.java

--- APCSA-2020/src/starfighter/AlienHorde.java
+++ APCSA-2020/src/starfighter/AlienHorde.java
@@ -16,22 +16,65 @@ public class AlienHorde
 
 	public AlienHorde(int size)
 	{
+		aliens = new ArrayList<Alien>();
+		for (int i=0; i<size; i++)
+		{
+			add(new Alien(0, 0));
+		}
 	}
 
 	public void add(Alien al)
 	{
+		aliens.add(al);
 	}
 
 	public void drawEmAll( Graphics window )
 	{
+		for (int i=0; i<aliens.size(); i++)
+		{
+			aliens.get(i).draw(window);
+		}
 	}
 
 	public void moveEmAll()
 	{
+		for (int i=0; i<aliens.size(); i++)
+		{
+			Alien current=aliens.get(i);
+			if (i==0 || aliens.get(i-1).getY()!=current.getY()
+					|| current.getX()+aliens.get(i-1).getWidth()+50<aliens.get(i-1).getX()
+					|| current.getX()>aliens.get(i-1).getX()+aliens.get(i-1).getWidth()+50)
+			{
+				if (current.getSpeed()>0 && current.getX()>=800-current.getWidth()-current.getSpeed()
+						|| current.getSpeed()<0 && current.getX()<=-current.getSpeed())
+				{
+					current.setSpeed(-current.getSpeed());
+					current.setY(current.getY()+current.getHeight());
+				}
+				else current.setX(current.getX()+current.getSpeed());
+			}
+		}
 	}
 
 	public void removeDeadOnes(List<Ammo> shots)
 	{
+		Ammo shot = shots.get(i);
+			for (int j=0; j<aliens.size(); j++)
+			{
+				Alien alien = aliens.get(j);
+				if(shot.getX() < (alien.getX() + alien.getWidth() + Math.abs(shot.getSpeed()))
+							&& shot.getX() >= (alien.getX() - shot.getWidth() - Math.abs(shot.getSpeed()))
+							&& shot.getX()<=alien.getX()+alien.getWidth()
+							&& shot.getY()<(alien.getY()+alien.getHeight())
+							&& (shot.getY()+shot.getHeight())>alien.getY())
+				{
+					aliens.remove(j);
+					shots.remove(i);
+					j--;
+					i--;
+				}
+			}
+		}
 	}
 
 	public String toString()
